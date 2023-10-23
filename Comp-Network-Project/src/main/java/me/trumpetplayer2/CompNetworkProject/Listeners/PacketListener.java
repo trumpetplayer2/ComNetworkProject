6f7ef8a6b6c2ea192b6691ec2d46d5736ce9d24a
trumpetplayer2 <trumpetplayer2@outlook.com>
2023-10-23 16:43:40 -0400

    Updated Packet Listener

--- Comp-Network-Project/src/main/java/me/trumpetplayer2/CompNetworkProject/Listeners/PacketListener.java
+++ Comp-Network-Project/src/main/java/me/trumpetplayer2/CompNetworkProject/Listeners/PacketListener.java
@@ -6,9 +6,19 @@ import org.bukkit.event.Listener;
 
 import com.comphenix.protocol.events.PacketEvent;
 
+import me.trumpetplayer2.CompNetworkProject.Main;
+
 public class PacketListener implements Listener {
 	@EventHandler
 	public void packetRecieved(PacketEvent e) {
+	    //Get player from event
 		Player p = e.getPlayer(); 
+		//Get instance and check if player is loaded
+		Main instance = Main.getInstance();
+		//Get if player is listening to packets, if not, return
+		if(instance.getPlayerState(p.getUniqueId().toString()) == null) {return;}
+		if(!instance.getPlayerState(p.getUniqueId().toString())) {return;}
+		//Send packet as a string to the player
+		p.sendMessage(e.getPacket().toString());
 	}
 }
