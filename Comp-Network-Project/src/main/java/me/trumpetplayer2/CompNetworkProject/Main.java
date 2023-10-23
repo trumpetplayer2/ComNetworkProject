b6e131c04477bb98c2bb4ac8e75a1199f01fb2b7
trumpetplayer2 <trumpetplayer2@outlook.com>
2023-10-23 17:31:44 -0400

    Bug Fixes and Packet Reading

--- Comp-Network-Project/src/main/java/me/trumpetplayer2/CompNetworkProject/Main.java
+++ Comp-Network-Project/src/main/java/me/trumpetplayer2/CompNetworkProject/Main.java
@@ -4,7 +4,17 @@ import java.util.HashMap;
 
 import javax.annotation.Nullable;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
 
 import me.trumpetplayer2.CompNetworkProject.Commands.NetworkPacketCommand;
 import me.trumpetplayer2.CompNetworkProject.Listeners.*;
@@ -12,6 +22,7 @@ import me.trumpetplayer2.CompNetworkProject.Listeners.*;
 public class Main extends JavaPlugin{
     //Instance variable allows linking back to main plugin later on
 	private static Main instance;
+	private ProtocolManager protocolManager;
 	//Hashmap that contains reference to players UUID as 
 	//well as a boolean for if packet feed is enabled
 	HashMap<String, Boolean> playerState = new HashMap<String, Boolean>();
@@ -32,6 +43,9 @@ public class Main extends JavaPlugin{
         getCommand("networkpacket").setTabCompleter(new NetworkPacketCommand());
         //Run register listener function. This is used for organization purposes
         registerListeners();
+        //Register ProtocolManager
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        enablePacketListeners();
     }
     //Default Disable. This is ran if Server stopped,
     //Plugin failed to load, or reload occurs
@@ -44,7 +58,6 @@ public class Main extends JavaPlugin{
     public void registerListeners() {
     	//Register listeners here. Follow format if needed to use
     	this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), instance);
-    	this.getServer().getPluginManager().registerEvents(new PacketListener(), instance);
     }
     
     //Get player state based on their UUID.
@@ -65,5 +78,33 @@ public class Main extends JavaPlugin{
     	if(State == null) {return;}
     	//Set the Hashmap if State is valid
     	playerState.put(UUID, State);
+    }
+    
+    public void enablePacketListeners() {
+        //Add the packet Listener to watch the event
+        protocolManager.addPacketListener(new PacketAdapter(
+                this, //Connect to this plugin
+                ListenerPriority.NORMAL, //Listener Priority is Normal, as it isn't important when it happens
+                PacketType.Play.Server.REL_ENTITY_MOVE
+                ) {
+            @Override
+            public void onPacketSending(PacketEvent e) {
+                PacketContainer packet = e.getPacket();
+                Player p = e.getPlayer();
+                Boolean pState = getPlayerState(p.getUniqueId().toString());
+                if(pState == null) {return;}
+                if(!pState) {return;}
+                //Lets get the packets, this event sends:
+                //Entity ID - VarInt[0]
+                //Delta X - Short[0]
+                //Delta Y - Short[1]
+                //Delta Z - Short[2]
+                //OnGround - Boolean[0]
+                p.sendMessage("Entity ID: " + packet.getIntegers().getValues().get(0) + 
+                        " Delta X: " + packet.getShorts().getValues().get(0) + 
+                        " Delta Y: " + packet.getShorts().getValues().get(1) +
+                        " Delta Z: " + packet.getShorts().getValues().get(2));
+            }
+        });
     }
 }
